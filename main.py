e9c582fba58056eb96c8552dc0a0c78c82ac20e1
alex07 <ac25harr@siena.edu>
2025-11-06 20:00:48 -0500

    Updated main.py to take first initial of inputed first name and first five letters of last name and a random number between 10 and 99 to create a user ID, for example, AHARRI79

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-# Add appropriate comments...such as your name, the date...
+# Alex Harriman
+#11/6/25
+#requests the first and last name as inputs to output a custom user ID
 
 import random
+import math
 
-# makeID takes two ...
-# and returns...
-def makeID(     ,     ):
+# makeID takes first and last name 
+# and returns the first initial of the first name and last five initials of the lastname ending with a random number between 10 and 99
+def makeID( firstName, lastName ):
     # Extract the first inital of their first name
-
-    # Extract the first five ...
-
+    firstInit = firstName[0].upper()
+    # Extract the first five letters of last name
+    lastFirstFive = lastName[0:5].upper()
     # Get a random number between 10 and 99
-    myNumber = random.range(   ,   )
+    myNumber = random.randint(10,99)
 
     # Put it all together (concatenate)
-
+    ID = firstInit + lastFirstFive + str(myNumber)
     return ID
 
 
@@ -23,13 +26,13 @@ def main():
     firstName = input("What is your first name? ")
 
     # Ask for their last name
-
+    lastName = input("What is your last name? ")
 
     # Call makeID
-    newID =
+    newID = makeID(firstName, lastName)
 
     # Print out the newID
-
+    print (newID)
 
 
 # Function definitions above
